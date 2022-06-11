6f4eed28a86972dbe22bfe712dfcc62a20315402
gelomamo <angelfdezfdez87@gmail.com>
2022-06-11 10:18:19 +0000

    primer commit

--- src/models.py
+++ src/models.py
@@ -8,23 +8,40 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Usuarios(Base):
+    __tablename__ = 'Usuarios'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(10), nullable=False)
+    fecha_subscripcion = Column(String(10), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Comment(Base):
+    __tablename__ = 'Comment'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    comment_text = Column(String(250), nullable=False)
+    Usuario_id = Column(Integer, ForeignKey('Usuarios.id'))
+    Post_id = Column(Integer, ForeignKey('Post.id'))
+
+class Post(Base):
+    __tablename__ = 'Post'
+    id = Column(Integer, primary_key=True)
+    Usuario_id = Column(Integer, ForeignKey('Usuarios.id'))
+
+class Media(Base):
+    __tablename__ = 'Media'
+    id = Column(Integer, primary_key=True)
+    type = Column(Number, nullable=False)
+    url = Column(String(250), nullable=False)
+    Post_id = Column(Integer, ForeignKey('Post.id'))
+
+class Follower(Base):
+    __tablename__ = 'Post'
+    user_from_id = Column(Integer, ForeignKey('Usuarios.id'))
+    user_to_id = Column(Integer, ForeignKey('Usuarios.id'))
 
     def to_dict(self):
         return {}
